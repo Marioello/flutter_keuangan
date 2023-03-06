8c59aef2e6349db0a38d96f59f786487d38f78f2
Marioello <toshiro89@gmail.com>
2023-03-06 17:50:43 +0700

    Separate Code

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,9 @@ import 'package:provider/provider.dart';
 
 import 'configs/firebase';
 import 'constants.dart';
-import 'models/arguments.dart';
 import 'models/model.dart';
 import 'pages/dashboard.dart';
-import 'pages/history.dart';
-import 'pages/not_found.dart';
+import 'route.dart';
 import 'services/database.dart';
 
 Future<void> main() async {
@@ -27,53 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: Dashboard.route,
-      onGenerateRoute: (settings) {
-        if (settings.name == '/history') {
-          final args = settings.arguments as HistoryArguments;
-          if (args.uid.isNotEmpty) {
-            return pageRoute(
-              MultiProvider(
-                providers: [
-                  StreamProvider<List<Member>>.value(
-                    initialData: const [],
-                    value: DatabaseService(uid: '').members,
-                  ),
-                  StreamProvider<List<Payment>>.value(
-                    initialData: const [],
-                    value: DatabaseService(uid: args.uid).payments,
-                  ),
-                ],
-                child: History(uid: args.uid),
-              ),
-            ); // Pass it to BarPage.
-          }
-        }
-        if (settings.name.toString().contains('/history?')) {
-          final settingsUri = Uri.parse(settings.name ?? '');
-          final uid = settingsUri.queryParameters['uid'].toString();
-          final name = settingsUri.queryParameters['name'].toString();
-          if (uid.isNotEmpty) {
-            return pageRoute(
-              MultiProvider(
-                providers: [
-                  StreamProvider<List<Member>>.value(
-                    initialData: const [],
-                    value: DatabaseService(uid: '').members,
-                  ),
-                  StreamProvider<List<Payment>>.value(
-                    initialData: const [],
-                    value: DatabaseService(uid: uid).payments,
-                  ),
-                ],
-                child: History(uid: uid),
-              ),
-            ); // Pass it to BarPage.
-          }
-        }
-
-        // Let `onUnknownRoute` handle this behavior.
-        return pageRoute(const NotFound());
-      },
+      onGenerateRoute: (settings) => routeValidation(settings),
       routes: {
         Dashboard.route: (context) {
           return MultiProvider(
@@ -103,12 +55,6 @@ class MyApp extends StatelessWidget {
       // ),
       // home: History(data: memberData[0]),
       debugShowCheckedModeBanner: false,
-    );
-  }
-
-  MaterialPageRoute<dynamic> pageRoute(Widget page) {
-    return MaterialPageRoute(
-      builder: (_) => page,
     );
   }
 }
---
+++ lib/route.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'models/arguments.dart';
+import 'models/model.dart';
+import 'pages/history.dart';
+import 'pages/not_found.dart';
+import 'services/database.dart';
+
+MaterialPageRoute<dynamic> pageRoute(Widget page) =>
+    MaterialPageRoute(builder: (_) => page);
+
+routeValidation(RouteSettings settings) {
+  if (settings.name == '/history') {
+    final args = settings.arguments as HistoryArguments;
+    if (args.uid.isNotEmpty) {
+      return pageRoute(
+        MultiProvider(
+          providers: [
+            StreamProvider<List<Member>>.value(
+              initialData: const [],
+              value: DatabaseService(uid: '').members,
+            ),
+            StreamProvider<List<Payment>>.value(
+              initialData: const [],
+              value: DatabaseService(uid: args.uid).payments,
+            ),
+          ],
+          child: History(uid: args.uid),
+        ),
+      ); // Pass it to BarPage.
+    }
+  }
+  if (settings.name.toString().contains('/history?')) {
+    final settingsUri = Uri.parse(settings.name ?? '');
+    final uid = settingsUri.queryParameters['uid'].toString();
+    if (uid.isNotEmpty) {
+      return pageRoute(
+        MultiProvider(
+          providers: [
+            StreamProvider<List<Member>>.value(
+              initialData: const [],
+              value: DatabaseService(uid: '').members,
+            ),
+            StreamProvider<List<Payment>>.value(
+              initialData: const [],
+              value: DatabaseService(uid: uid).payments,
+            ),
+          ],
+          child: History(uid: uid),
+        ),
+      ); // Pass it to BarPage.
+    }
+  }
+
+  // Let `onUnknownRoute` handle this behavior.
+  return pageRoute(const NotFound());
+}
